e4b1a7b6c939a6b640298c36ed399be612a71c0c
Rasmus <rasmus.helgesson@elev.ga.lbs.se>
2018-10-05 15:53:49 +0200

    Color changing

---
+++ Assets/ColoronShip.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoronShip : MonoBehaviour {
+    public SpriteRenderer rend;
+    public Color newcolor;
+    bool Blue;
+    bool Green;
+	// Use this for initialization
+	void Start () {
+        Blue = false;
+        Green = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Blue = true;
+
+            if (Blue == true)
+            {
+                newcolor = rend.color = new Color(0, 0, 1);
+
+            }
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Green = true;
+
+            if (Green == true)
+            {
+                newcolor = rend.color = new Color(0, 1, 0);
+
+            }
+
+        }
+    }
+}
---
+++ Assets/SpaceShipscript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipscript : MonoBehaviour {
+    [Range(-720, 720)]
+    public float rotationSpeed;
+
+    
+    public Transform other;
+
+    // Use this for initialization
+    void Start () {
+        
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+
+        }
+        transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
+
+
+
+
+
+
+
+
+
+    }
+}
