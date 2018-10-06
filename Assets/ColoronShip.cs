d566a30b510bd4c4d5d035adedcf2ecbdb30c47d
Rasmus <rasmus.helgesson@elev.ga.lbs.se>
2018-10-06 13:52:15 +0200

    RandomColor

--- Assets/ColoronShip.cs
+++ Assets/ColoronShip.cs
@@ -7,14 +7,18 @@ public class ColoronShip : MonoBehaviour {
     public Color newcolor;
     bool Blue;
     bool Green;
+    bool Randomcolor;
+    
 	// Use this for initialization
 	void Start () {
         Blue = false;
         Green = false;
+        Randomcolor = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // Ändrar färg till Blå
         if (Input.GetKey(KeyCode.D))
         {
             Blue = true;
@@ -26,6 +30,7 @@ public class ColoronShip : MonoBehaviour {
             }
 
         }
+        // Ändrar färg till Grön
         if (Input.GetKey(KeyCode.A))
         {
             Green = true;
@@ -33,6 +38,17 @@ public class ColoronShip : MonoBehaviour {
             if (Green == true)
             {
                 newcolor = rend.color = new Color(0, 1, 0);
+
+            }
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Randomcolor = true;
+
+            if (Randomcolor == true)
+            {
+                newcolor = rend.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 
             }
 
--- Assets/SpaceShipscript.cs
+++ Assets/SpaceShipscript.cs
@@ -5,41 +5,59 @@ using UnityEngine;
 public class SpaceShipscript : MonoBehaviour {
     [Range(-720, 720)]
     public float rotationSpeed;
-
-    
-    public Transform other;
-
+    public bool halfspeed;
+   public Transform other;
+    public float Timer;
+    private float variabel = 1;
+   
     // Use this for initialization
     void Start () {
+        halfspeed = false;
         
-
 
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        // Objektet åker fram
+        transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
 
+        // Svänger höger
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
-
-
+            
         }
+        // Svänger Vänster
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
 
         }
-        transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
+       
+        // Åker långsammare när man trycker ner S tangenten
+        if (Input.GetKey(KeyCode.S))
+        {
+            halfspeed = true;
 
+            if (halfspeed == true)
+            {
+                transform.Translate(-2.5f * Time.deltaTime, 0, 0, Space.Self);
+            }
 
+        }
 
+        // Timer ökar varenda sekund
+        Timer += Time.deltaTime;
+        if (Timer > variabel && Timer < variabel + 0.2)
+        {
+            Debug.Log(string.Format("Timer:"));
 
-
-
-
-
-
+            print(Timer);
+            variabel = (variabel + 1);
+            
+        } 
+        
     }
 }
